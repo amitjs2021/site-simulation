dfe4f246edcbd5caadfea674073ac13fcde07ec8
Amit Sharma <amitsharma@192-168-1-101.tpgi.com.au>
2021-09-08 13:04:49 +1000

    server and api updated reac and upload

--- api/server.js
+++ api/server.js
@@ -1,55 +1,49 @@
-const express = require("express");
-const multer = require("multer");
-const cors = require("cors");
-const fileUpload = require("express-fileupload");
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const cors = require('cors')
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 4500
 
-app.use(express());
-app.use(cors());
-app.use(express.static("local-store"));
+// middle ware
+app.use(express.static('uploadfiles')); //to access the files in public folder
+app.use(cors()); // it enables all cors requests
 app.use(fileUpload());
 
-//setting multer storage location and use filename from file object
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "local-store");
-  },
-  filename: function (req, file, cb) {
-    const { originalname } = file;
-    cb(null, originalname);
-  },
+
+
+//file reading from path for now only sitemap.txt is fixed file name use
+var usersFilePath = path.join(__dirname, 'uploadfiles/sitemap.txt');
+console.log(usersFilePath)
+
+// file reader api in SYNC mode - we can convert this in non-blocking mode if require
+app.get('/api/v1/maps', function (req, res) {
+  var readable = fs.createReadStream(usersFilePath);
+  readable.pipe(res)
+
 });
 
-const upload = multer({ storage: storage });
-
-app.get("/", (req, res) => {
-  res.send("server running ");
-});
-
-app.post("/api/v1/upload", upload.single("file"), (req, res) => {
-  res.json({ status: 200, body: "Upload Done !" });
-});
-
-// Upload Endpoint
-app.post("/upload", (req, res) => {
-  if (req.files === null) {
-    return res.status(400).json({ msg: "No file uploaded" });
+// file upload api
+app.post('/api/v1/upload', (req, res) => {
+  if (!req.files) {
+    return res.status(500).send({ msg: "file is not found" })
   }
+  // accessing the file
+  const myFile = req.files.file;
 
-  const file = req.files.file;
-
-  file.mv(`${__dirname}/local-store/${file.name}`, (err) => {
+  //  mv() method places the file inside uplaodfiles directory
+  myFile.mv(`${__dirname}/uploadfiles/${myFile.name}`, function (err) {
     if (err) {
-      console.error(err);
-      return res.status(500).send(err);
+      console.log(err)
+      return res.status(500).send({ msg: "Error occured" });
     }
-
-    res.json({ fileName: file.name, filePath: `/local-dire/${file.name}` });
+    // returing the response with file path and name
+    return res.send({ name: myFile.name, path: `/${myFile.name}` });
   });
-});
+})
 
 app.listen(PORT, () => {
-  console.log(`server runnitn on PORT ${PORT}`);
-});
+  console.log('server is running at port 4500');
+})
